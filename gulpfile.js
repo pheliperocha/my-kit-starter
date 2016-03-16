be45ce3950da858e3c7312f12bc070aae900f083
Phelipe Rocha <phelipeafonso@gmail.com>
2016-03-16 15:07:05 -0300

    Improve gulpfile, add plato report and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,87 @@
-var gulp = require('gulp'),
-	uglifyjs = require('gulp-uglify'),
-	uglifycss = require('gulp-uglifycss'),
-	autoprefixer = require('gulp-autoprefixer'),
-	imagemin = require('gulp-imagemin');
+'use strict';
 
-// Task to uglify js files
-gulp.task('scripts', function() {
-	gulp.src('./dev/src/*.js')
-		.pipe(uglifyjs())
-		.on("error", errorLog)
-		.pipe(gulp.dest('./dist/js/'));
-});
+var gulp, gulpLoadPlugins;
 
-// Task the styles files of the project
-// Add autoprefixer in css styles to fit in the 5% most useful browser
-// And minify the files
-gulp.task('styles', function () {
-	gulp.src('./dev/css/*.css')
-		.pipe(autoprefixer({
-			browsers: ['> 5%'],
-		}))
-		.pipe(uglifycss())
-		.on("error", errorLog)
-		.pipe(gulp.dest('./dist/css/'));
-});
+gulp = require('gulp');
+gulpLoadPlugins = require('gulp-load-plugins');
 
-// Task to minify the images files
-// Need review
-gulp.task('image', function() {
-	gulp.src('./img/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('./img/'));
-});
-
-// Watch the changes to then apply the uglify to the respectives files
-gulp.task('watch', function() {
-	gulp.watch("./dev/src/*.js", ['uglifyJS']);	
-	gulp.watch("./dev/css/*.css", ['uglifyCSS']);	
-});
+const $ = gulpLoadPlugins();
 
 // Function to catch erros and prevent to stop the watch event
 function errorLog(error) {
-	console.error.bind(error);
-	this.emit('end');
+     console.error.bind(error);
+     this.emit('end');
 }
 
-gulp.task('default', ['scripts', 'styles', 'watch']);
+// Task to uglify js files
+gulp.task('scripts', function () {
+     gulp.src('./dev/src/**/*.js')
+          .pipe($.concat('script.min.js'))
+          .pipe($.uglify())
+          .on("error", errorLog)
+          .pipe(gulp.dest('./dist/js/'));
+});
+
+// Task the styles files of the project
+// Compiler the sass files and concat
+// Add autoprefixer in css styles to fit in the 1% most useful browser
+// And minify
+gulp.task('styles', function () {
+     const AUTOPREFIXER_BROWSERS = [
+          'ie >= 10',
+          'ie_mob >= 10',
+          'ff >= 30',
+          'chrome >= 34',
+          'safari >= 7',
+          'opera >= 23',
+          'ios >= 7',
+          'android >= 4.4',
+          'bb >= 10'
+     ];
+
+
+
+     gulp.src([
+          './dev/styles/**/*.scss',
+          './dev/styles/**/*.css'
+     ])
+     .pipe($.sass())
+     .pipe($.concat('style.min.css'))
+     .on("error", errorLog)
+     .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+     .pipe($.uglifycss())
+     .pipe(gulp.dest('./dist/styles/'));
+});
+
+// Optimize images
+gulp.task('images', () =>
+     gulp.src('./dev/img/**/*')
+          .pipe($.cache($.imagemin({
+               progressive: true,
+               interlaced: true
+          })))
+          .pipe(gulp.dest('dist/images'))
+          .pipe($.size({title: 'images'}))
+);
+
+gulp.task('plato', function () {
+     return gulp.src('./dev/js/**/*.js')
+          .pipe($.plato('./report', {
+          jshint: {
+               options: {
+                    strict: true
+               }
+          },
+          complexity: {
+               trycatch: true
+          }
+     }));
+});
+
+// Watch the changes of scss and js files
+gulp.task('watch', function () {
+     gulp.watch("./dev/src/**/*.js", ['scripts']);
+     gulp.watch("./dev/styles/**/*.scss", ['styles']);
+});
+
+gulp.task('default', ['scripts', 'styles', 'images', 'watch']);
